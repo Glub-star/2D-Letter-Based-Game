aab861df7f1aed39fe0e0fb0814880a69e5c750d
Glub-star <135123481+Glub-star@users.noreply.github.com>
2024-09-17 15:08:54 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#2D Text Game
+
 import random, os
 
 window_size = 'mode 50,20'
 
 os.system(window_size)
 
-while True:
-    print("x")
+class Player():
+    def __init__(self):
+        self.position = (25,10)
+        self.sprite = "█"
+    
+player = Player()
